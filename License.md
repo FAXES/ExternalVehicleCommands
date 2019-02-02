658c06a1db6687b71805ceb7a1f75b2b01e982e8
FAXES <31368070+FAXES@users.noreply.github.com>
2019-02-02 19:53:57 +1100

    1.1

--- License.md
+++ License.md
@@ -2,7 +2,7 @@
 
 (Below the term "product" is used to describe the script / modification)
 
-This script is freeware and is not to be exploited for personal, financial or commercial gain. This product is provided without any form of warranty.
+This product is freeware and is not to be exploited for personal, financial or commercial gain. This product is provided without any form of warranty.
 Therefore, responsibility for any damages caused by this product or its misuse rest solely with the user, as the author(s) will accept no liability.
 
 - This copy of terms must remain in its original state and must not be modified in anyway
@@ -11,9 +11,11 @@ Therefore, responsibility for any damages caused by this product or its misuse r
 
 - Do not re-release with out permission (just ask for permission)
 
-- Do not redistribute to any other sites. This script is only to be published on verified sites by FAXES
+- Do not redistribute to any other sites. This product is only to be published on verified sites by FAXES
 
 - Editing / abusing these terms will result in action
+
+- Do not take credit for product. Removing credits from inside the products file(s) is prohibited
 
 ### Summery
 So basically you can edit anything with the product (except the terms), but you can not release the product nor release the edited version without written permission from FAXES. 
--- client.lua
+++ client.lua
@@ -2,6 +2,12 @@
 -- External Vehicle Commands, Made by FAXES --
 ----------------------------------------------
 
+function ShowInfo(text)
+	SetNotificationTextEntry("STRING")
+	AddTextComponentString(text)
+	DrawNotification(false, false)
+end
+
 RegisterCommand("trunk", function(source, args, raw)
     local ped = GetPlayerPed(-1)
     local veh = GetVehiclePedIsUsing(ped)
@@ -12,22 +18,22 @@ RegisterCommand("trunk", function(source, args, raw)
     if IsPedInAnyVehicle(ped, false) then
         if GetVehicleDoorAngleRatio(veh, door) > 0 then
             SetVehicleDoorShut(veh, door, false)
-            TriggerEvent("chatMessage", "^*[Vehicle] Trunk Closed.")
+            ShowInfo("[Vehicle] ~g~Trunk Closed.")
         else	
             SetVehicleDoorOpen(veh, door, false, false)
-            TriggerEvent("chatMessage", "^*[Vehicle] Trunk Opened.")
+            ShowInfo("[Vehicle] ~g~Trunk Opened.")
         end
     else
         if distanceToVeh < 6 then
             if GetVehicleDoorAngleRatio(vehLast, door) > 0 then
                 SetVehicleDoorShut(vehLast, door, false)
-                TriggerEvent("chatMessage", "^*[Vehicle] Trunk Closed.")
+                ShowInfo("[Vehicle] ~g~Trunk Closed.")
             else
                 SetVehicleDoorOpen(vehLast, door, false, false)
-                TriggerEvent("chatMessage", "^*[Vehicle] Trunk Opened.")
+                ShowInfo("[Vehicle] ~g~Trunk Opened.")
             end
         else
-            TriggerEvent("chatMessage", "^*[Vehicle] Too far away from vehicle.")
+            ShowInfo("[Vehicle] ~y~Too far away from vehicle.")
         end
     end
 end)
@@ -42,22 +48,69 @@ RegisterCommand("hood", function(source, args, raw)
     if IsPedInAnyVehicle(ped, false) then
         if GetVehicleDoorAngleRatio(veh, door) > 0 then
             SetVehicleDoorShut(veh, door, false)
-            TriggerEvent("chatMessage", "^*[Vehicle] Hood Closed.")
+            ShowInfo("[Vehicle] ~g~Hood Closed.")
         else	
             SetVehicleDoorOpen(veh, door, false, false)
-            TriggerEvent("chatMessage", "^*[Vehicle] Hood Opened.")
+            ShowInfo("[Vehicle] ~g~Hood Opened.")
         end
     else
         if distanceToVeh < 4 then
             if GetVehicleDoorAngleRatio(vehLast, door) > 0 then
                 SetVehicleDoorShut(vehLast, door, false)
-                TriggerEvent("chatMessage", "^*[Vehicle] Hood Closed.")
+                ShowInfo("[Vehicle] ~g~Hood Closed.")
             else	
                 SetVehicleDoorOpen(vehLast, door, false, false)
-                TriggerEvent("chatMessage", "^*[Vehicle] Hood Opened.")
+                ShowInfo("[Vehicle] ~g~Hood Opened.")
             end
         else
-            TriggerEvent("chatMessage", "^*[Vehicle] Too far away from vehicle.")
+            ShowInfo("[Vehicle] ~y~Too far away from vehicle.")
+        end
+    end
+end)
+
+RegisterCommand("door", function(source, args, raw)
+    local ped = GetPlayerPed(-1)
+    local veh = GetVehiclePedIsUsing(ped)
+    local vehLast = GetPlayersLastVehicle()
+    local distanceToVeh = GetDistanceBetweenCoords(GetEntityCoords(ped), GetEntityCoords(vehLast), 1)
+    
+    if args[1] == "1" then -- Front Left Door
+        door = 0
+    elseif args[1] == "2" then -- Front Right Door
+        door = 1
+    elseif args[1] == "3" then -- Back Left Door
+        door = 2
+    elseif args[1] == "4" then -- Back Right Door
+        door = 3
+    else
+        door = nil
+        ShowInfo("Usage: ~n~~b~/door [door]")
+        ShowInfo("~y~Possible doors:")
+        ShowInfo("1(Front Left Door), 2(Front Right Door)")
+        ShowInfo("3(Back Left Door), 4(Back Right Door)")
+    end
+
+    if door ~= nil then
+        if IsPedInAnyVehicle(ped, false) then
+            if GetVehicleDoorAngleRatio(veh, door) > 0 then
+                SetVehicleDoorShut(veh, door, false)
+                TriggerEvent("^*[Vehicle] ~g~Door Closed.")
+            else	
+                SetVehicleDoorOpen(veh, door, false, false)
+                TriggerEvent("^*[Vehicle] ~g~Door Opened.")
+            end
+        else
+            if distanceToVeh < 4 then
+                if GetVehicleDoorAngleRatio(vehLast, door) > 0 then
+                    SetVehicleDoorShut(vehLast, door, false)
+                    TriggerEvent("[Vehicle] ~g~Door Closed.")
+                else	
+                    SetVehicleDoorOpen(vehLast, door, false, false)
+                    TriggerEvent("[Vehicle] ~g~Door Opened.")
+                end
+            else
+                TriggerEvent("[Vehicle] ~y~Too far away from vehicle.")
+            end
         end
     end
 end)
